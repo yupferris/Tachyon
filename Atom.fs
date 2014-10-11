378282e20031846b181252ea89c9b9e1ef7a68e6
ferris <jake@outracks.com>
2014-10-11 02:02:19 +0200

    threw together a quick, stripped-down implementation of clojure's atoms

---
+++ Atom.fs
@@ -0,0 +1,25 @@
+﻿module Tachyon.Atom
+    open System
+    open System.Threading
+
+    type Atom<'a when 'a : not struct> =
+        {
+            get : unit -> 'a
+            swap : ('a -> 'a) -> 'a
+        }
+
+    let atom obj =
+        let r = ref obj
+
+        let get () = !r
+        let rec swap f =
+            let oldVal = !r
+            let newVal = f oldVal
+            let result = Interlocked.CompareExchange<_>(r, newVal, oldVal)
+            if Object.ReferenceEquals(result, oldVal) then newVal
+            else swap f
+
+        {
+            get = get
+            swap = swap
+        }
