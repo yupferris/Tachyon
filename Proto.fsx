900196aa885219072ab0619e28fda9995b4500fa
ferris <jake@outracks.com>
2014-10-11 04:45:01 +0200

    haha ok, starting to feel how these compose; this is fucking rad :D

--- Proto.fsx
+++ Proto.fsx
@@ -43,7 +43,22 @@ let map<'a, 'b when 'a : not struct and 'b : not struct> (f : 'a -> 'b) (a : Ato
     a.addWatch (fun _ x -> ret.swap(fun _ -> f x) |> ignore) |> ignore
     ret
 
-let a = atom "hasn't updated yet"
-let b = map (fun (x : string) -> sprintf "the length of the last value was %d" x.Length) a
-a.swap (fun _ -> "123")
+let filter f = map (fun x -> if f x then x else Unchecked.defaultof<_>)
+
+let choose f =
+    map
+        (fun x ->
+            match f x with
+            | Some x -> x
+            | _ -> Unchecked.defaultof<_>)
+
+let foldp<'a, 'b when 'a : not struct and 'b : not struct> (f : 'a -> 'b -> 'b) (b : 'b) (a : Atom<'a>) =
+    let ret = atom(f (a.get()) b)
+    a.addWatch (fun _ x -> ret.swap(fun p -> f x p) |> ignore) |> ignore
+    ret
+
+let a = atom ""
+let b = foldp (fun x y -> y + " " + x) "" a
+
+Array.iter (fun x -> a.swap (fun _ -> x) |> ignore) ("these are some words".Split(' '))
 b.get()
