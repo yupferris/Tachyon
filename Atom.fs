b0e54b9f527b00e7aed8b0ddf90e8528c62479d8
ferris <jake@outracks.com>
2014-10-11 02:47:04 +0200

    reworked basic atom implementation to support simple watches

--- Atom.fs
+++ Atom.fs
@@ -2,24 +2,29 @@
     open System
     open System.Threading
 
-    type Atom<'a when 'a : not struct> =
-        {
-            get : unit -> 'a
-            swap : ('a -> 'a) -> 'a
-        }
+    type Atom<'a when 'a : not struct>(value : 'a) =
+        let r = ref value
+        let watches = ref Map.empty
 
-    let atom obj =
-        let r = ref obj
-
-        let get () = !r
-        let rec swap f =
+        let rec compareAndSwap r f =
             let oldVal = !r
             let newVal = f oldVal
             let result = Interlocked.CompareExchange<_>(r, newVal, oldVal)
-            if Object.ReferenceEquals(result, oldVal) then newVal
-            else swap f
+            if Object.ReferenceEquals(result, oldVal) then (oldVal, newVal)
+            else compareAndSwap r f
 
-        {
-            get = get
-            swap = swap
-        }
+        member x.get () = !r
+        member x.swap f =
+            let oldValue, newValue = compareAndSwap r f
+            Map.iter (fun key watch -> watch key x oldValue newValue) !watches
+            newValue
+
+        member x.getWatches () = !watches
+        member x.addWatch key watch =
+            compareAndSwap watches (fun w -> Map.add key watch w) |> ignore
+            x
+        member x.removeWatch key =
+            compareAndSwap watches (fun w -> Map.remove key w) |> ignore
+            x
+
+    let atom value = new Atom<_(*, _*)>(value)
