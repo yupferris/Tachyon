fef386e6373dd57b35f116666ea179d4f333a2f9
ferris <jake@outracks.com>
2014-10-11 04:04:51 +0200

    still a fucking mess, but it kindof works sorta maybe XD

--- Proto.fsx
+++ Proto.fsx
@@ -18,7 +18,7 @@ type AtomicReference<'a when 'a : not struct> (value : 'a) =
 
 type Atom<'a, 'b when 'a : not struct and 'b : comparison> (value : 'a) =
     let r = new AtomicReference<'a>(value)
-    let watches = new AtomicReference<Map<'b, 'b -> Atom<'a, 'b> -> 'a -> 'a -> unit>>(Map.empty)
+    let watches = new AtomicReference<Map<'b, 'a -> 'a -> unit>>(Map.empty)
 
     member x.get = r.get
     member x.swap f =
@@ -26,7 +26,7 @@ type Atom<'a, 'b when 'a : not struct and 'b : comparison> (value : 'a) =
             let oldValue = r.get()
             let newValue = f oldValue
             if r.compareAndSwap oldValue newValue then
-                Map.iter (fun key watch -> watch key x oldValue newValue) (watches.get())
+                Map.iter (fun key watch -> watch oldValue newValue) (watches.get())
                 newValue
             else aux()
         aux()
@@ -39,15 +39,13 @@ type Atom<'a, 'b when 'a : not struct and 'b : comparison> (value : 'a) =
         watches.swap (fun w -> Map.remove key w) |> ignore
         x
 
-    interface IComparable
-        with member x.CompareTo other = 1 // lol
-
-let atom value = new Atom<_, _>(value)
+let atom value = new Atom<_, int>(value)
 
 let map<'a, 'b when 'a : not struct and 'b : not struct> f (a : Atom<'a, _>) =
-    let ret = new Atom<'b, _>(f (a.get()))
-    a.addWatch ret (fun _ _ _ x -> ret.swap(fun _ -> f x) |> ignore) |> ignore
+    let ret = atom(f (a.get()))
+    a.addWatch 0 (fun _ x -> ret.swap(fun _ -> f x) |> ignore) |> ignore
     ret
 
 let a = atom "hasn't updated yet"
-//let b = map (fun _ -> printfn "YEP IT'S WOOD") a
+let b = map (fun (x : string) -> sprintf "the length of the last value was %d" x.Length) a
+b.get()
